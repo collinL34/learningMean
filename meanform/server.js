b6a5eca8a2c2e640c62ea8fc922b803c23f08bc1
collinl34 <collinlshelby@gmail.com>
2017-02-20 00:02:14 -0800

    :sparkles: project skeleton create and basic route add to project

--- meanform/server.js
+++ meanform/server.js
@@ -1,4 +1,23 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const method-override = require('method-override');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+
+let port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser.urlencoded({ 'extended': 'true' })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
+
+require('./app/routes.js')(app);
+
+
+app.listen(port);
+
+console.log('Magic happens at Port ' + port);
+
+exports = module.exports = app;
