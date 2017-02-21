b8d0be016f82b1c9a65de470ac7af025e9d50e0d
collinl34 <collinlshelby@gmail.com>
2017-02-20 21:46:18 -0800

    passport integrate into app along with users model methods add

--- login/app_api/models/users.js
+++ login/app_api/models/users.js
@@ -1,4 +1,5 @@
 const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -13,3 +14,20 @@ const userSchema = new mongoose.Schema({
     hash: String,
     salt: String
 });
+
+userSchema.methods.setPassword = function() {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+userSchema.methods.generateJwt = function() {
+  var expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    name: this.name,
+    exp: parseInt(expiry.getTime() / 1000),
+  }, "MY_SECRET"); // DO NOT KEEP YOUR SECRET IN THE CODE!
+};
