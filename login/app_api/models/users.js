37759c4ed45555f68ad601eb537187b719cff14e
collinl34 <collinlshelby@gmail.com>
2017-02-20 16:46:59 -0800

    gulp integrated into project

---
+++ login/app_api/models/users.js
@@ -0,0 +1,13 @@
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    hash: String,
+    salt: String
+});
---
+++ login/gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const watch = require('gulp-watch');
+const sourcemaps = require('gulp-sourcemaps');
+const ngHtml2Js = require("gulp-ng-html2js");
+
+gulp.task('scripts', function() {
+    gulp.src(['./app_client/**/*.js', '!./app_client/**/*.test.js', '!./app_client/app.min.js'])
+        .pipe(sourcemaps.init())
+        .pipe(concat('./app.min.js'))
+        .pipe(uglify({ mangle: true }))
+        .pipe(gulp.dest('app_client'))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('app_client'));
+});
+
+gulp.task('watch', function() {
+    watch(['./app_client/**/*.js', '!./app_client/**/*.test.js', '!./app_client/app.min.js'], function() {
+        gulp.start('scripts');
+    });
+});
+
+gulp.task('default', ['scripts', 'watch']);
